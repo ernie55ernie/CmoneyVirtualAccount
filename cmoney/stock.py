950dd1f60299bacf44992775f09ff5de860a0148
Yu-Wei Chang <b01505025@g.ntu.edu.tw>
2024-12-13 12:16:41 +0800

    fix: new login with two factor auth

--- cmoney/stock.py
+++ cmoney/stock.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from enum import Enum
 
+from bs4 import BeautifulSoup
 import requests
 import json
 import re
@@ -22,45 +23,93 @@ class VirtualStockAccount():
         輸入帳號(email)密碼(password)來登入，並設定每個request的延遲時間是wait_time
 
         """
-
+        login_url = 'https://www.cmoney.tw/identity/account/login'
+        two_factor_url = "https://www.cmoney.tw/identity/account/login/two-factor-options"
+#         headers = {
+#             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+#             'Referer': login_url
+#         }
         self.ses = requests.Session()
-        res = self.ses.get('https://www.cmoney.tw/member/login/')
+        res = self.ses.get(login_url)#, headers=headers)
+        soup = BeautifulSoup(res.text, 'html.parser')
+
+        # Extract the CSRF token from the login page
+        csrf_token = soup.find('input', {'name': '__RequestVerificationToken'})['value']
+
         self.wait_time = wait_time
-
-        # get view state
-        viewstate = re.search( 'VIEWSTATE"\s+value=.*=', res.text )
-        viewstate = viewstate.group()
-
-        # get eventvalidation
-        eventvalidation = re.search( 'EVENTVALIDATION"\s+value=.*\w', res.text )
-        eventvalidation = eventvalidation.group( )
-
+        
         time.sleep(self.wait_time)
 
-        res = self.ses.post('https://www.cmoney.tw/member/login/', {
-                '__VIEWSTATE': viewstate[18:],
-            '__EVENTVALIDATION': eventvalidation[24:],
-            'ctl00$ContentPlaceHolder1$account': email,
-            'ctl00$ContentPlaceHolder1$pw': password,
-            'ctl00$ContentPlaceHolder1$loginBtn': '登入',
-            'ctl00$ContentPlaceHolder1$check': 'on',
+        res = self.ses.post(login_url, data={ # , headers=headers
+            # '__VIEWSTATE': viewstate[18:],
+            # '__EVENTVALIDATION': eventvalidation[24:],
+            'Account': email,
+            'Password': password,
+            '__RequestVerificationToken': csrf_token,
+            'RememberMe': 'true',
+            'ClientId': 'cmmainweb',
+            'RedirectUri': '/member/login/signin-oidc.aspx',
+            'ReturnUrl': '~/authorize/callback?client_id=cmmainweb&redirect_uri=https://outpost.cmoney.tw/member/login/signin-oidc.aspx&response_type=none&scope=',
+            'LogoUri': 'https://image.cmoney.tw/servicetest/swagger/1675180800/d1202faa-8464-4a42-a77e-eb16348f8508.png'
         })
-
         time.sleep(self.wait_time)
+        
+        # Check if 2FA page is encountered
+        if '透過雙重認證保護您的帳戶' in res.text:
+            soup = BeautifulSoup(res.text, 'html.parser')
+
+            # Extract CSRF token from 2FA page
+            csrf_token_2fa = soup.find('input', {'name': '__RequestVerificationToken'})['value']
+            return_url = soup.find('input', {'name': 'ReturnUrl'})['value'] if soup.find('input', {'name': 'ReturnUrl'}) else ''#'~/authorize/callback?client_id=cmmainweb&amp;redirect_uri=https://outpost.cmoney.tw/member/login/signin-oidc.aspx&amp;response_type=none&amp;scope='
+            remember_me = soup.find('input', {'name': 'RememberMe'})['value'] if soup.find('input', {'name': 'RememberMe'}) else 'true'
+            # Bypass Two-Factor Authentication
+            two_factor_payload = {
+                'DontAskAgain': 'true',     # Don't ask again
+                'IsEnable': 'false',        # Skip enabling 2FA
+                '__RequestVerificationToken': csrf_token_2fa,
+                'ReturnUrl': return_url,    # Add the required ReturnUrl
+                'RememberMe': remember_me
+            }
+
+            # Submit the 2FA bypass request
+            res = self.ses.post(two_factor_url, data=two_factor_payload)#, headers=headers
+            print("Login successful after bypassing 2FA!")
+
+        else:
+            print("Login successful without 2FA.")
 
         res = self.ses.get('https://www.cmoney.tw/vt/main-page.aspx')
+        
+        # Parse the response content
+        soup = BeautifulSoup(res.text, 'html.parser')
+        # Extract ChooseMemberId and __RequestVerificationToken
+        choose_member_id = soup.find('input', {'id': 'ChooseMemberId'}).get('value', '')
+        request_verification_token = soup.find('input', {'name': '__RequestVerificationToken'}).get('value', '')
+        return_url = soup.find('input', {'name': 'ReturnUrl'})['value'] if soup.find('input', {'name': 'ReturnUrl'}) else ''
 
+        # Send the form submission request
+        submit_url = 'https://www.cmoney.tw/identity/account/select'
+        form_data = {
+            'ChooseMemberId': choose_member_id,
+            '__RequestVerificationToken': request_verification_token,
+            'ReturnUrl': return_url,
+        }
+
+        # Send the POST request
+        res = self.ses.post(submit_url, data=form_data)
+        
+        res = self.ses.get('https://www.cmoney.tw/vt/main-page.aspx')
         aids = re.findall('aid=[\d]*', res.text )
-        print(aids)
         aids = [a.split('=')[1] for a in aids]
         aids = [a for a in aids if a.isdigit()]
         self.aids = aids
         if self.aids:
             self.aid = aids[0]
+            print('accounts', aids)
+            print('current account', self.aid)
         else:
-            raise Exception('Cannot open account due to incorrect account name or password')
-        print('accounts', aids)
-        print('current account', self.aid)
+            print('Require manually set account ID')
+#             raise Exception('Cannot open account due to incorrect account name or password')
 
     def get_price(self, sid):
 
@@ -171,8 +220,9 @@ class VirtualStockAccount():
         res = self.ses.get('https://www.cmoney.tw/vt/ashx/accountdata.ashx', params={
             'act': 'InventoryDetail',
             'aid': self.aid,
+            '_': '1572343162391'
         })
-
+        print(res.text)
         return json.loads(res.text)
 
     def listEntrust(self, f, s):
